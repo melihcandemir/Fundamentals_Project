20b49515a924a4c8ca470f57d0d407142b0b17a7
Melih Can Demir <melihcanndemir@gmail.com>
2024-12-31 16:41:31 +0300

    Sayı bulma oyunu eklendi

--- Program.cs
+++ Program.cs
@@ -6,9 +6,6 @@
 // 1 - Rastgele Sayı Bulma Oyunu
 // 2 - Hesap Makinesi
 // 3 - Ortalama Hesaplama
-// küçük karaktere çevrilen string değerin kayıp yaşamaması için eklendi
-using System.Globalization; // yapay zekadan yardım alındı 
-Console.OutputEncoding = System.Text.Encoding.UTF8; // konsol utf-8 e göre çalışması için
 
 // kodlar düzenli gözüksün diye alt alta yazdım
 Console.WriteLine("---------------HOŞGELDİNİZ---------------");
@@ -19,25 +16,79 @@ Console.WriteLine("3 - Ortalama Hesaplama");
 Console.Write("Seçiminiz: ");
 string? secim = Console.ReadLine() ?? string.Empty; // değer girilmez ise boş sayılır
 
-foreach (char c in secim)
-{
-    Console.WriteLine($"Karakter: {c} | Unicode: {(int)c}");
-}
-Console.WriteLine("-------------------------");
+secim = secim.ToLower(); // girilen değerleri küçültür
+secim = secim.Trim(); // trim ile string değerin başındaki ve sonundaki boşluklar silinir
 
-secim = secim.ToLower(new CultureInfo("tr-TR")); // girilen değerleri küçültür utf-8 e uygun olarak
-secim = secim.Trim(); // trim ile boşluklar silinir
+// sayı bulma oyunu
+// Rastgele Sayı Bulma Oyunu
+// Bilgisayar bir rastgele sayı belirlesin (örneğin 1 ile 100 arasında).
+// Kullanıcıdan bu sayıyı tahmin etmesini isteyin.
+// Kullanıcı her tahmininde, bilgisayarın seçtiği sayıya yakınlığını kontrol edin ve ipucu verin (daha yüksek veya daha düşük).
+// Kullanıcının 5 canı olsun (5 tahmin hakkı). Her tahmin sonrası kalan canını gösterin.
+// Kullanıcı doğru tahminde bulunursa oyunu bitirin ve tebrik edin.
+// Eğer 5 tahmin hakkı dolarsa, doğru sayıyı gösterin ve oyunu bitirin.
 
-foreach (char c in secim)
+// sayı bulma oyunu
+void Oyun()
 {
-    Console.WriteLine($"Karakter: {c} | Unicode: {(int)c}");
+    Console.WriteLine("----------------------------Sayı Bulma Oyununa Hoşgeldiniz----------------------------");
+    // rastgele sayı
+    Random rastgele = new Random();
+    int sayi = rastgele.Next(1,5);
+
+    int i = 0; // tahmin hakkı
+
+    // kullanıcı tahmini
+    Console.Write($"Üretilen rastgele sayıyı tahmin ediniz. 1 ile 100 arasında. Tahmin hakkınız 5\n{i + 1}.  Tahmininiz: ");
+    int tahmin = Convert.ToInt32(Console.ReadLine());
+
+    do
+    {
+        i++;
+
+        if (sayi < tahmin)
+        {
+            Console.WriteLine("----------------");
+            Console.Write($"Tahmininizden daha küçük bir sayı giriniz.\n{5 - i} hakkınız kaldı.\n{i + 1}.- Tahmininizi giriniz: ");
+            tahmin = Convert.ToInt32(Console.ReadLine());
+        }
+        else if (sayi > tahmin)
+        {
+            Console.WriteLine("----------------");
+            Console.Write($"Tahmininizden daha büyük bir sayı giriniz.\n{5 - i} hakkınız kaldı.\n{i + 1}.- Tahmininizi giriniz: ");
+            tahmin = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        if (sayi == tahmin)
+        {
+            Console.WriteLine($"Tahmin sayınız: {tahmin}\nBulmanız gereken sayı: {sayi}\n{i + 1}. seferde doğru tahmin ettiniz....");
+            Console.WriteLine("----------------");
+            break;
+        }
+    
+        if (i == 4)
+        {
+            Console.WriteLine("----------------");
+            Console.WriteLine($"5 denemede bulamadınız. Tahmin etmeye çalıştığınız sayı: {sayi}");
+            Console.WriteLine("----------------");
+            break;
+        }
+            
+        
+
+
+    } while(i < 4);
+
+
+    Console.WriteLine("Oyundan çıkılıyor..");
 }
+
 
 switch (secim)
 {
     case "1":
     case "rastgele sayı bulma oyunu":
-        Console.WriteLine("başarılı-1");
+        Oyun();
         break;
 
     case "2":
